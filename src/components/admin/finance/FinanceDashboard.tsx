2476d5af6bd120b3c7fa94b8bf8e72ca2c8e50c6
Brayan C Molina Sanchez <admin@22electronicgroup.com>
2025-09-19 06:26:12 +0000

    Try fixing this error: `Runtime Error: Error: Can't find variable: React

--- src/components/admin/finance/FinanceDashboard.tsx
+++ src/components/admin/finance/FinanceDashboard.tsx
@@ -1,5 +1,7 @@
+
 "use client";
 
+import * as React from "react";
 import { useState, useMemo, FC } from "react";
 import { Expense, Sale, RepairOrder, Product, Consignor, ConsignorPayment, CashSession, Account } from "@/types";
 import { Button } from "@/components/ui/button";
